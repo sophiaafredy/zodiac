2f3c708a475372cb37141dbf294ec2bc04ef7eb6
Debasis Bhattacharya <debasisb@hawaii.edu>
2020-02-18 06:02:33 +0000

    Fixed and tested http code

--- index.js
+++ index.js
@@ -22,4 +22,5 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+  console.log('My Zodiac Sign is ' + myZodiacSign)
 });
